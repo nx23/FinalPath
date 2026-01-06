c8a3a0ee5a20dac5db78ec006ac26b99149b1341
Numero23 <gbotareli@gmail.com>
2026-01-06 16:16:04 -0300

    add initial game configuration and implement enemy movement along paths

--- go.mod
+++ go.mod
@@ -5,10 +5,29 @@ go 1.25.5
 require github.com/hajimehoshi/ebiten/v2 v2.9.7
 
 require (
+	dario.cat/mergo v1.0.2 // indirect
+	github.com/air-verse/air v1.63.6 // indirect
+	github.com/bep/godartsass/v2 v2.5.0 // indirect
+	github.com/bep/golibsass v1.2.0 // indirect
 	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gohugoio/hugo v0.149.1 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/spf13/cast v1.9.2 // indirect
+	github.com/tdewolff/parse/v2 v2.8.3 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
 )
+
+tool github.com/air-verse/air
--- main.go
+++ main.go
@@ -1,30 +1,149 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type Game struct{}
+type Game struct {
+	maps []Map
+}
+
+type Window struct {
+	Width  int
+	Height int
+	Title  string
+}
+
+type Path struct {
+	StartX float32
+	StartY float32
+	EndX   float32
+	EndY   float32
+}
+
+type Map []Path
+
+type Enemy struct {
+	PositionX        float32
+	PositionY        float32
+	Speed            float32
+	currentPathIndex int
+}
+
+var firstMap = Map{
+	{StartX: 350, StartY: 0, EndX: 350, EndY: 150},
+	{StartX: 350, StartY: 150, EndX: 550, EndY: 150},
+	{StartX: 550, StartY: 150, EndX: 550, EndY: 350},
+	{StartX: 600, StartY: 350, EndX: 150, EndY: 350},
+	{StartX: 150, StartY: 350, EndX: 150, EndY: 600},
+}
+
+var newWindow = Window{
+	Width:  800,
+	Height: 600,
+	Title:  "Final Path v1.0",
+}
+
+var enemy = &Enemy{
+	PositionX:        362.5, // Centered in the path (350 + 12.5)
+	PositionY:        0,
+	Speed:            2,
+	currentPathIndex: 0,
+}
+
+func createMap(screen *ebiten.Image, m Map) {
+	const pathWidth float32 = 50
+	for _, path := range m {
+		width := path.EndX - path.StartX
+		height := path.EndY - path.StartY
+
+		// If vertical path (width is 0), add pathWidth
+		if width == 0 {
+			width = pathWidth
+		}
+
+		// If horizontal path (height is 0), add pathWidth
+		if height == 0 {
+			height = pathWidth
+		}
+
+		vector.FillRect(screen, path.StartX, path.StartY, width, height, color.White, false)
+	}
+}
+
+func (enemy *Enemy) followPath(m Map) {
+	if enemy.currentPathIndex >= len(m) {
+		return
+	}
+
+	const pathWidth float32 = 50
+	const enemySize float32 = 25
+	const offset = (pathWidth - enemySize) / 2 // Center offset: 12.5
+
+	path := m[enemy.currentPathIndex]
+
+	// Vertical movement
+	if path.StartX == path.EndX {
+		if enemy.PositionY-offset < path.EndY {
+			enemy.PositionY += enemy.Speed
+		} else {
+			enemy.currentPathIndex++
+			fmt.Printf("Path %d completed\n", enemy.currentPathIndex)
+		}
+	} else if path.StartY == path.EndY { // Horizontal movement
+		// Move right or left depending on direction
+		if path.EndX > path.StartX {
+			// Move right
+			if enemy.PositionX-offset < path.EndX {
+				enemy.PositionX += enemy.Speed
+			} else {
+				enemy.currentPathIndex++
+				fmt.Printf("Path %d completed\n", enemy.currentPathIndex)
+			}
+		} else {
+			// Move left
+			if enemy.PositionX-offset > path.EndX {
+				enemy.PositionX -= enemy.Speed
+			} else {
+				enemy.currentPathIndex++
+				fmt.Printf("Path %d completed\n", enemy.currentPathIndex)
+			}
+		}
+	}
+}
 
 func (g *Game) Update() error {
+	enemy.followPath(firstMap)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// Background
+	vector.FillRect(screen, 0, 0, float32(screen.Bounds().Dx()), float32(screen.Bounds().Dy()), color.Black, false)
+
+	// Map
+	createMap(screen, g.maps[0])
+
+	// Enemy
+	vector.FillRect(screen, enemy.PositionX, enemy.PositionY, 25, 25, color.RGBA{255, 0, 0, 255}, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return newWindow.Width, newWindow.Height
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	g := &Game{
+		maps: []Map{firstMap},
+	}
+	ebiten.SetWindowSize(newWindow.Width, newWindow.Height)
+	ebiten.SetWindowTitle(newWindow.Title)
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
